c693c480598c77d23d46498b68ba6d0589a69d34
Leon Hunter <leonhunter@Leons-MacBook-Pro.local>
2019-04-15 12:24:38 -0400

    update;

--- src/main/java/rocks/zipcode/io/collections/CollectionUtils.java
+++ src/main/java/rocks/zipcode/io/collections/CollectionUtils.java
@@ -1,9 +1,17 @@
 package rocks.zipcode.io.collections;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class CollectionUtils {
+    public static List<?> toList(Collection collection) {
+        List<?> list = new ArrayList<>();
+        list.addAll(collection);
+        return list;
+    }
+
     public static Boolean contains(Collection<? extends Collection<?>> nestedCollection, Collection<?> collection) {
         for(Collection<?> subCollection : nestedCollection) {
             if(subCollection.containsAll(collection)) {
