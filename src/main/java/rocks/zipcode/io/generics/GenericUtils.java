3af559f377d6b913cef22fd0c693040fbdd00f1b
Leon Hunter <leonhunter@Leons-MacBook-Pro.local>
2019-04-15 13:02:21 -0400

    update;

--- src/main/java/rocks/zipcode/io/generics/GenericUtils.java
+++ src/main/java/rocks/zipcode/io/generics/GenericUtils.java
@@ -5,9 +5,9 @@ import java.util.Collection;
 import java.util.List;
 
 public class GenericUtils {
-    public static  <T> T[] toArray(Collection<T> set) {
-        List<T> list = new ArrayList<>(set);
-        T[] toR = (T[]) new Object[set.size()];
+    public static  <T> T[] toArray(Collection<T> collection) {
+        List<T> list = new ArrayList<>(collection);
+        T[] toR = (T[]) new Object[collection.size()];
         for (int i = 0; i < list.size(); i++) {
             toR[i] = list.get(i);
         }
